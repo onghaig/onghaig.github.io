53d5089cac7afdccb154a9e5c75ca91a8c981c0a
Gavin Onghai <onghaig@gmail.com>
2025-08-04 23:05:02 -0400

    Created compenents and began working on aboutme.

---
+++ src/components/aboutme.tsx
@@ -0,0 +1,19 @@
+export default function AboutMe() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-left">
+            <div>
+                <h1 className="text-4xl font-bold mb-4">
+                    Hi there, I'm Gavin!
+                </h1>
+            </div>
+            <div>
+                <h2 className="text-xl font-medium max-w-2xl">
+                    My name is Gavin Ong Hai (王海), and I am a sophomore at Yale University studying
+                    Electrical Engineering and Computer Science. Much of my past work has been in 
+                    material science and chemistry. Currently, I am more interested in working on software and hardware development,
+                    as well as learning about financial markets. I hope you enjoy looking through my website!
+                </h2>
+            </div>
+        </div>
+    )
+}
