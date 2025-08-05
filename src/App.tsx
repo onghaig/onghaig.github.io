2109d783fc7af0b0f364bf06e2c998779415d08c
Gavin Onghai <onghaig@gmail.com>
2025-08-04 23:19:28 -0400

    Created skills component and worked on resume component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AboutMe from './components/aboutme'
+import Resume from './components/resume'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,10 @@ function App() {
   return (
     <>
       <AboutMe/>
+      <Projects/>
+      <Resume/>
+      <Contact/>
+      <Blog/>
     </>
   )
 }
---
+++ src/components/resume.tsx
@@ -0,0 +1,17 @@
+export default function Resume() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-left">
+            <div>
+                <h1 className="text-4xl font-bold mb-4">
+                    My Resume
+                </h1>
+            </div>
+            <div>
+                <h2 className="text-xl font-medium max-w-2xl">
+                    Here you can find my resume, detailing my education, work experience, and skills.
+                    Feel free to download it or contact me for more information!
+                </h2>
+            </div>
+        </div>
+    )
+}
