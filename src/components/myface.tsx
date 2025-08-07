3b6446be6282eb55a0e5bd0e3c5bbe21f2c867a9
Gavin Onghai <onghaig@gmail.com>
2025-08-07 19:27:34 -0400

    Updated face image and about me. Added yale icon. and implemented said icon.

--- src/components/myface.tsx
+++ src/components/myface.tsx
@@ -1,11 +1,13 @@
-import myFace from "../assets/myface.jpg";
+import myFace from "../assets/myface.png";
 
 export default function MyFace() {
     return (
+        <div className="relative">
         <img 
             src={myFace}
             alt="Gavin Ong Hai"
-            className="items-start wrap-anywhere rounded-full w-96 h-128 object-cover ml-12"
+            className="items-start wrap-anywhere rounded-4xl w-96 h-128 object-cover ml-12"
           />
-    )
+        </div>
+    )   
 }
