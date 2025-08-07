385a3d22e63f865ceef04a00396a8ac58e77f929
Gavin Onghai <onghaig@gmail.com>
2025-08-07 14:07:16 -0400

    updated spacing between section for project, will do the same later.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,6 @@
 export default function Projects() {
     return (
-        <section id="projects" className="relative px-8 py-16">
+        <section id="projects" className="relative px-8 py-25">
                 <h1 className="text-4xl font-bold mb-4">
                     My Projects
                 </h1>
