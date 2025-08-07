9f6aac267e78c8d0ec622cea1adeccd345dc2a15
Gavin Onghai <onghaig@gmail.com>
2025-08-07 14:06:12 -0400

    Updated font type, and size of heading3's

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -2,17 +2,17 @@ import MyFace from "./myface";
 
 export default function AboutMe() {
   return (
-    <section id="about" className="relative h-screen flex items-center lg:flex-row">
+    <section id="about" className="relative px-8 py-32 flex items-center lg:flex-row">
         <div className="flex flex-col items-start max-w-3xl">
           <h1 className="text-4xl font-bold">Hi there, I'm Gavin!</h1>
-        <h2 className="text-xl text-left font-medium max-w-3xl mt-4 indent-10">
-          My name is Gavin Ong Hai (王海), and I am a sophomore at Yale University
+        <h3 className="text-xl text-left font-light max-w-3xl mt-4 indent-10">
+          My name is Gavin Ong Hai (王海加文), and I am a sophomore at Yale University
           studying Electrical Engineering and Computer Science. Much of my past
           work has been in material science and chemistry. Currently, I am more
           interested in working on software and hardware development, as well as
           learning about financial markets. I hope you enjoy looking through my
           website!
-        </h2>
+        </h3>
         </div>
       <MyFace/>
     </section>
