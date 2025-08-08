f8a15d451f9ba5433e89ebb12ef08e0a05e9e15d
Gavin Onghai <onghaig@gmail.com>
2025-08-08 00:10:51 -0400

    adjusted image placement, overlapping face.

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,12 +1,10 @@
 import MyFace from "./myface";
-import YaleIcon from "../assets/Yale_University_Shield.png";
 
 export default function AboutMe() {
   return (
     <section id="about" className="relative px-8 py-32 flex items-center lg:flex-row">
         <div className="flex flex-col items-start max-w-3xl">
-          <h1 className="text-4xl font-bold">Hi there, I'm Gavin! <img src={YaleIcon} alt="" aria-hidden="true"
-          className="inline-block h-[2em] w-[2em] align-text-bottom gap-1" /></h1>
+          <h1 className="text-4xl font-bold">Hi there, I'm Gavin! </h1>
         <h3 className="text-xl text-left font-light max-w-3xl mt-4 indent-10 ">
           My name is Gavin Ong Hai (王海加文), and I am a sophomore at Yale University
           studying Electrical Engineering and Computer Science. Much of my past
--- src/components/myface.tsx
+++ src/components/myface.tsx
@@ -1,4 +1,5 @@
 import myFace from "../assets/myface.png";
+import YaleIcon from "../assets/Yale_University_Shield.png";
 
 export default function MyFace() {
     return (
@@ -8,6 +9,8 @@ export default function MyFace() {
             alt="Gavin Ong Hai"
             className="items-start wrap-anywhere rounded-4xl w-96 h-128 object-cover ml-12"
           />
+        <img src={YaleIcon} alt="" aria-hidden="true"
+          className="absolute right-0 top-100 fill-white bg-white border-1 rounded-lg h-24 w-24" />
         </div>
     )   
 }
