317dd40ccf4cbc2bf48eba9ccd71fc743f3daa92
Gavin Onghai <onghaig@gmail.com>
2025-08-10 13:48:55 -0400

    Fixed spacing for elements. Added basic skills intro

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,12 +1,9 @@
 export default function Skills() {
     return (
-            <section id="skills" className="relative flex flex-col justify-center px-4 lg:px-8">
+            <section id="skills" className="relative min-h-screen flex flex-col justify-start px-4 lg:px-8 pt-16 lg:pt-32 scroll-mt-0">
                 <h1 className="text-3xl lg:text-4xl font-bold mb-4 text-center lg:text-left">
                     Skills
                 </h1>
-                <p className="text-lg lg:text-xl lg:text-left font-medium leading-relaxed mb-4">
-                    Here are some technologies I have worked with:
-                </p>
         </section>
     )
 }
